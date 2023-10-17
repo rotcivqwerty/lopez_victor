76a204921c7db79b8a704cf05e6d2a7fb73aab5a
Victor Lopez <victorlopeza380@gmail.com>
2023-10-17 15:51:30 +0800

    Activity1 submission

---
+++ Activity1/registerPage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<script>
+    function storeData() {
+        let Username = document.getElementById("username").value;
+        let Password = document.getElementById("password").value;
+        let Email = document.getElementById("email").value;
+
+        localStorage.setItem("username", Username);
+        localStorage.setItem("email", Email);
+        localStorage.setItem("password", Password);
+
+        alert("Data Registered");
+    }
+</script>
+
+<body>
+    <div class="register">
+        <h1>Register</h1>
+        <form method="post" autocomplete="off" onsubmit="storeData()">
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Username" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Password" id="password" required>
+            <label for="email">
+                <i class="fas fa-envelope"></i>
+            </label>
+            <input type="email" name="email" placeholder="Email" id="email" required>
+            <input type="submit" value="Register">
+        </form>
+    </div>
+</body>
+
+</html>
